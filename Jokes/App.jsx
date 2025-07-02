0f65ce7fe9198b5d3926914465f42402081124a0
Muhammad Sharjeel Ali <msharjeelali@yahoo.com>
2025-07-03 00:21:23 +0500

    Delete Jokes directory

--- Jokes/App.jsx
+++
@@ -1,5 +0,0 @@
-export default function App(){
-    return <main>
-        This is App component.
-    </main>
-}
--- Jokes/index.jsx
+++
@@ -1,7 +0,0 @@
-import {createRoot} from "react"
-import App from "App"
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-    <App />
-)
