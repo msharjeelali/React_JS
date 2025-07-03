8dbf4b9dc97be9d2e6289e12f45979f152cb9328
Sharjeel Ali <msharjeelali@yahoo.com>
2025-07-03 16:27:40 +0500

    jokes challenege passing non string props

--- src/src/components/Joke.jsx
+++ src/src/components/Joke.jsx
@@ -1,7 +1,10 @@
-export default function Joke({setup, punchline}){
-    return <article class="joke">
+export default function Joke({setup, punchline, upvotes, downvotes}){
+    return <article className="joke">
         <h3>Joke</h3>
-        <p><span>Setup:</span> {setup}</p>
-        <p><span>Punchline:</span> {punchline}</p>
+        {/* If setup and punchline is truthy value, only then render it */}
+        {setup && <p><span>Setup:</span> {setup}</p>}
+        {punchline && <p><span>Punchline:</span> {punchline}</p>}
+        <p><span>Up Votes: {upvotes}</span></p>
+        <p><span>Down Votes: {downvotes}</span></p>
     </article>
 }
--- src/src/components/JokeProject.jsx
+++ src/src/components/JokeProject.jsx
@@ -4,25 +4,35 @@ import "./Joke.css"
 export default function JokeProject(){
     const jokes = [{
         "setup": "I got my daughter a fridge for her birthday.",
-        "punchline": "I can't wait to see her face light up when she opens it."
+        "punchline": "I can't wait to see her face light up when she opens it.",
+        "upvotes": 5,
+        "downvotes": 2
     }, {
         "setup": "How did the hacker escape the police?",
-        "punchline": "He just ransomware!"
+        "punchline": "He just ransomware!",
+        "upvotes": 7,
+        "downvotes": 0
     }, {
         "setup": "Why don't pirates travel on mountain roads?",
-        "punchline": "Scurvy."
+        "punchline": "Scurvy.",
+        "upvotes": 4,
+        "downvotes": 2
     }, {
         "setup": "Why do bees stay in the hive in the winter?",
-        "punchline": "Swarm."
+        "punchline": "Swarm.",
+        "upvotes": 5,
+        "downvotes": 2
     }, {
         "setup": "What's the best thing about Switzerland?",
-        "punchline": "I don't know, but the flag is a big plus!"
+        "punchline": "I don't know, but the flag is a big plus!",
+        "upvotes": 10,
+        "downvotes": 3
     }];
 
     return (<section>
         <h2>Jokes Challenge</h2>
         {jokes.map(element => (
-            <Joke setup={element.setup} punchline={element.punchline} />
+            <Joke setup={element.setup} punchline={element.punchline} upvotes={element.upvotes} downvotes={element.downvotes} />
         ))}
         <p>This is a challenge from <a href="https://scrimba.com/home">Scrimba</a> <a href="https://scrimba.com/learn-react-c0e">Learn React</a> course.</p>
     </section>);
