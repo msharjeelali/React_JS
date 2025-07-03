9b2823c3a8886d143135f4a5a6cee92022291b4e
Sharjeel Ali <msharjeelali@yahoo.com>
2025-07-03 18:24:05 +0500

    update in passing prop data

--- src/src/components/JokeProject.jsx
+++ src/src/components/JokeProject.jsx
@@ -1,39 +1,15 @@
 import Joke from "./Joke"
+import jokesData from "./jokesData";
 import "./Joke.css"
 
 export default function JokeProject(){
-    const jokes = [{
-        "setup": "I got my daughter a fridge for her birthday.",
-        "punchline": "I can't wait to see her face light up when she opens it.",
-        "upvotes": 5,
-        "downvotes": 2
-    }, {
-        "setup": "How did the hacker escape the police?",
-        "punchline": "He just ransomware!",
-        "upvotes": 7,
-        "downvotes": 0
-    }, {
-        "setup": "Why don't pirates travel on mountain roads?",
-        "punchline": "Scurvy.",
-        "upvotes": 4,
-        "downvotes": 2
-    }, {
-        "setup": "Why do bees stay in the hive in the winter?",
-        "punchline": "Swarm.",
-        "upvotes": 5,
-        "downvotes": 2
-    }, {
-        "setup": "What's the best thing about Switzerland?",
-        "punchline": "I don't know, but the flag is a big plus!",
-        "upvotes": 10,
-        "downvotes": 3
-    }];
-
+    const jokesElement = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} upvotes={joke.upvotes} downvotes={joke.downvotes} />
+    })
+    
     return (<section>
         <h2>Jokes Challenge</h2>
-        {jokes.map(element => (
-            <Joke setup={element.setup} punchline={element.punchline} upvotes={element.upvotes} downvotes={element.downvotes} />
-        ))}
+        {jokesElement}
         <p>This is a challenge from <a href="https://scrimba.com/home">Scrimba</a> <a href="https://scrimba.com/learn-react-c0e">Learn React</a> course.</p>
     </section>);
 }
---
+++ src/src/components/jokesData.jsx
@@ -0,0 +1,28 @@
+export default [
+    {
+        "setup": "I got my daughter a fridge for her birthday.",
+        "punchline": "I can't wait to see her face light up when she opens it.",
+        "upvotes": 5,
+        "downvotes": 2
+    }, {
+        "setup": "How did the hacker escape the police?",
+        "punchline": "He just ransomware!",
+        "upvotes": 7,
+        "downvotes": 0
+    }, {
+        "setup": "Why don't pirates travel on mountain roads?",
+        "punchline": "Scurvy.",
+        "upvotes": 4,
+        "downvotes": 2
+    }, {
+        "setup": "Why do bees stay in the hive in the winter?",
+        "punchline": "Swarm.",
+        "upvotes": 5,
+        "downvotes": 2
+    }, {
+        "setup": "What's the best thing about Switzerland?",
+        "punchline": "I don't know, but the flag is a big plus!",
+        "upvotes": 10,
+        "downvotes": 3
+    }
+];
